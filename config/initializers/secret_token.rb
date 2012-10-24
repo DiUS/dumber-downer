619a95f3dc688026130add0cf9c8375a2081f78f
Tal Rotbart <redbeard@gmail.com>
2012-10-24 15:02:35 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DumberDowner::Application.config.secret_token = 'f2fd86287f64f6b64f06fbe8a708332335dd134331119fc808510feb638958d1fef94014dfc479060c9f4d9f1df8869caf605a03f095658475e1419d906339e6'
