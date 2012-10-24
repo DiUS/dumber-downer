302e2a26ad9cea2ce483a50063d797c4eec78f70
Tal Rotbart <redbeard@gmail.com>
2012-10-24 16:49:50 +1100

    Initial twitter support.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,4 +1,17 @@
 class ClientController < ApplicationController
   def index
+    @home_timeline = Twitter.home_timeline
+  rescue Twitter::Error::TooManyRequests => error
+    raise "Rate limited. Try again in #{error.rate_limit.reset_in} seconds."
   end
+
+  private
+
+  def twitter_client
+    @twitter_client ||= Twitter::Client.new(
+      :oauth_token => ENV['TWITTER_OAUTH_TOKEN'],
+      :oauth_token_secret => ENV['TWITTER_OAUTH_TOKEN_SECRET']
+    )
+  end
+
 end
